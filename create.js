e8915f5839455340013956785cf47e970d960707
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-04-01 14:39:16 +0000

    Debug message

--- create.js
+++ create.js
@@ -1,3 +1,8 @@
 module.exports = async (job) => {
     console.log('j', job);
+
+    var { ip, password, sshPort } = job.data;
+    var id = job.id;
+
+    console.log(`Creating ${id} with ${password} @ ${ip} -p ${sshPort}`);
 };
