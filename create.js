73e6e5ced6ca840c58c9ced0a22cb9bf19396c05
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-04-01 15:56:10 +0000

    E

--- create.js
+++ create.js
@@ -3,23 +3,14 @@ const sleep = ms => new Promise(r => setTimeout(r, ms));
 module.exports = async (job) => {
     console.log('j', job);
 
-    var { ip, password, sshPort } = job.data;
+    var data = job.data;
     var id = job.id;
 
-    console.log(`Creating ${id} with ${password} @ ${ip} -p ${sshPort}`);
+    console.log(`Creating ${id} with ${data.password} @ ${data.ip} -p ${data.sshPort}`);
 
-    await sleep(3*1000);
     await job.updateProgress('step 1');
 
-    await sleep(3*1000);
-    await job.updateProgress('step 2');
+    data.proxID = 500;
 
-    await sleep(5*1000);
-    await job.updateProgress('step 3');
-
-    console.log('Created!');
-
-    return {
-        proxID: 300
-    };
+    return data;
 };
