016b216ebd19a9f46d924184d67be35097796860
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-04-01 13:45:07 +0000

    Add redis port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const BullMQ = require('bullmq');
 const path = require('path');
 
 const { NODE_LOCATION, NODE_TYPE, NODE_NUMBER } = process.env;
-const { REDIS_HOST, REDIS_USERNAME, REDIS_PASSWORD } = process.env;
+const { REDIS_HOST, REDIS_USERNAME, REDIS_PASSWORD, REDIS_PORT } = process.env;
 
 const NodeID = `${NODE_LOCATION}-${String(NODE_TYPE)[0]}${NODE_NUMBER}`
 
@@ -16,7 +16,8 @@ const worker = new BullMQ.Worker(
         connection: {
             host: REDIS_HOST,
             username: REDIS_USERNAME,
-            password: REDIS_PASSWORD
+            password: REDIS_PASSWORD,
+            port: REDIS_PORT
         }
     }
 );
