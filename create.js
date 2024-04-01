8d036474f5a72259e0217b71849dfcec267bfbbd
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-04-01 16:15:49 +0000

    Whoops

--- create.js
+++ create.js
@@ -1,14 +1,16 @@
 const sleep = ms => new Promise(r => setTimeout(r, ms));
+const shell = require('shelljs');
 
 module.exports = async (job) => {
-    console.log('j', job);
-
     var data = job.data;
     var id = job.id;
 
-    console.log(`Creating ${id} with ${data.password} @ ${data.ip} -p ${data.sshPort}`);
+    console.log(`Creating ${id} with ${data.password} @ ${data.ip} -p ${data.sshPort}`, data);
 
-    await job.updateProgress('step 1');
+    await job.updateProgress('Hello');
+
+    var proxID = await shell.exec(`pvesh get /cluster/nextid`);
+    console.log('p', proxID);
 
     data.proxID = 500;
     data.ok = true;
