540d917b7855d2bee9639c26ebf2689ddfd29866
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-04-02 12:39:55 +0000

    Add simple actions

--- index.js
+++ index.js
@@ -11,9 +11,24 @@ const NodeID = `${NODE_LOCATION}-${String(NODE_TYPE)[0]}${NODE_NUMBER}`;
 console.log('> Connected as ' + NodeID);
 
 const processorFile = path.join(__dirname, 'create.js');
+const opsFile = path.join(__dirname, 'ops.js');
 const worker = new BullMQ.Worker(
     `${NodeID}_create`,
     processorFile,
+    {
+        connection: {
+            host: REDIS_HOST,
+            username: REDIS_USERNAME,
+            password: REDIS_PASSWORD,
+            port: REDIS_PORT
+        },
+        concurrency: 1
+    }
+);
+
+const OPSworker = new BullMQ.Worker(
+    `${NodeID}_ops`,
+    opsFile,
     {
         connection: {
             host: REDIS_HOST,
---
+++ ops.js
@@ -0,0 +1,38 @@
+const shell = require('shelljs');
+
+module.exports = async (job) => {
+
+    var data = job.data;
+
+    var returnData;
+    returnData = {
+        ok: true
+    };
+
+    if (!data.action) throw new Error('No action');
+
+    switch (action) {
+        case "start":
+            var id = data.proxID;
+            await shell.exec(`pct start ${id}`);
+            break;
+        case "stop":
+            var id = data.proxID;
+            await shell.exec(`pct shutdown ${id}`);
+            break;
+        case "kill":
+            var id = data.proxID;
+            await shell.exec(`pct stop ${id}`);
+            break;
+        case "restart":
+            var id = data.proxID;
+            await shell.exec(`pct restart ${id}`);
+            break;
+        default:
+            returnData.ok = false;
+            returnData.error = 'invalid action';
+    }
+
+    return returnData;
+
+};
