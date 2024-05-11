69cd43e44cb4a8d70cfb62575ba85a82ad36e45e
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-05-11 12:14:38 +0200

    Update create.js

--- create.js
+++ create.js
@@ -70,7 +70,7 @@ function getCreateCMD(id, ip, password, path, storage, data) {
 
     cmd += ` ${path} `
     cmd += `--swap=512 `;
-    cmd += `--hostname=vps${id} `;
+    cmd += `--hostname=alpine${id} `;
     cmd += `--memory=4096 `;
     cmd += `--cmode=shell `;
     cmd += `--net0 name=eth0,bridge=vmbr0,firewall=1,gw=10.5.0.1,ip=${ip}/16,rate=3 `;
