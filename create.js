e519e4be3c752ac57d80c0db1637afc60f37c3a4
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-06-16 11:33:53 +0200

    Reduce storage to 5 GB

--- create.js
+++ create.js
@@ -89,7 +89,7 @@ function getCreateCMD(id, ip, password, path, storage, data) {
     cmd += `--unprivileged=1 `;
     cmd += `--cores=1 `;
     cmd += `--features fuse=1,nesting=1,keyctl=1 `;
-    cmd += `--rootfs ${storage}:10`;
+    cmd += `--rootfs ${storage}:5`;
 
     return cmd;
 }
